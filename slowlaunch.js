2f639d9b130a1635d37c3286cfa4735417ae37ce
bornthatway <83725474+bornthatway@users.noreply.github.com>
2023-09-25 12:18:26 -0700

    Create slowlaunch.js

---
+++ slowlaunch.js
@@ -0,0 +1,43 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+ var hc = request.url;
+ if(hc=="/hc") {
+    console.log('received health check');
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Healthy");
+    return
+ }
+
+ response.writeHead(200, { "Content-Type": "text/plain" });
+ response.end("Hello 3002 :) !!");
+});
+
+console.log('Server sleeping');
+function sleep(ms) {
+  var start = Date.now();
+  while(Date.now() - start < ms) {
+    ;
+  }
+}
+
+var start = Date.now();
+var end = start + 80000; // 3 minutes
+for (var i = 0; ; i++) {
+  console.log(Date.now() + " " + i + " "+ factorial(i));
+  sleep(2000)
+  if (Date.now() > end) {
+    break;
+  }
+}
+
+function factorial(n) {
+  var result = 1;
+  for (var i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+console.log('Server awake');
+console.log('Server is now listening');
+server.listen(3002);
